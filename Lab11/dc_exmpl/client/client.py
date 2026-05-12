639031aba9760e7871a02a4df00cae0ae5fe1083
Пётр Нгуен <petya.nguen@bk.ru>
2026-05-12 10:26:06 +0300

    Update Lab11

---
+++ Lab11/dc_exmpl/client/client.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import urllib.request
+
+fp = urllib.request.urlopen("http://localhost:1234/")
+# 'encodedContent' соответствует закодированному ответу сервера
+encodedContent = fp.read()
+# 'decodedContent' соответствует раскодированному ответу сервера
+decodedContent = encodedContent.decode("utf8")
+# Выводим содержимое файла, полученного с сервера ('index.html').
+print(decodedContent)
+# Закрываем соединение с сервером.
+fp.close()
